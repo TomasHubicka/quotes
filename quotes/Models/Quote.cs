f15aab9fedf03f5011472eab8bdd6c2f7b232f6c
Tomáš Hubička <u17.409@pslib.cloud>
2020-10-01 13:22:27 +0200

    Add project files.

---
+++ quotes/Models/Quote.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quotes.Models
+{
+    public class Quote
+    {
+        string Text { get; set; }
+        DateTime Date { get; set; }
+        [JsonIgnore]
+        ICollection<QuoteTag> Tags { get; set; }
+    }
+}
